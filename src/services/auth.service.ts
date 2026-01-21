bd8d43dfbcc7239ba5ce16f74ce9596448a025af
Karissa Jacobsen <karissa.jacobsen@docusign.com>
2026-01-21 09:41:47 -0800

    add client credential support

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -20,6 +20,7 @@ export const generateAuthToken = (req: IReq<GenerateAuthTokenBody>, res: IRes) =
   const accessToken = jwt.sign({ type: 'access_token', sub: crypto.randomUUID(), email: `${crypto.randomUUID()}@test.com` }, env.JWT_SECRET_KEY, {
     expiresIn: 3600,
   });
+  
   const refreshToken = jwt.sign({ type: 'refresh_token' }, env.JWT_SECRET_KEY);
 
   const jwtResponse = {
@@ -29,9 +30,11 @@ export const generateAuthToken = (req: IReq<GenerateAuthTokenBody>, res: IRes) =
     refresh_token: refreshToken,
   };
 
-  const decodedAuthCode = decodeURIComponent(env.AUTHORIZATION_CODE.replace(/\+/g, '%20'));
-
-  if (req.body.grant_type === 'authorization_code' && (req.body.code === decodedAuthCode || req.body.code === env.AUTHORIZATION_CODE)) {
+  if (
+    req.body.grant_type === 'authorization_code' &&
+    typeof req.body.code === 'string' &&
+    decodeURIComponent(req.body.code.replace(/\+/g, '%20')) === env.AUTHORIZATION_CODE
+  ) {
     return res.json(jwtResponse);
   } else if (req.body.grant_type === 'refresh_token' && req.body.refresh_token) {
     const payload = jwt.verify(req.body.refresh_token, env.JWT_SECRET_KEY) as JwtPayload;
@@ -39,6 +42,26 @@ export const generateAuthToken = (req: IReq<GenerateAuthTokenBody>, res: IRes) =
       throw new Error();
     }
     return res.json(jwtResponse);
+  } else if(req.body.grant_type === 'client_credentials') {
+
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader?.startsWith('Basic ')){
+      throw new Error();
+    }
+
+    const base64Credentials = authHeader.split(' ')[1];
+    const decoded = Buffer.from(base64Credentials, 'base64').toString('utf-8');
+    const [clientId, clientSecret] = decoded.split(':');
+
+    const decodedClientId = decodeURIComponent(clientId.replace(/\+/g, '%20'));
+    const decodedClientSecret = decodeURIComponent(clientSecret.replace(/\+/g, '%20'));
+
+    if(decodedClientId === env.OAUTH_CLIENT_ID && decodedClientSecret === env.OAUTH_CLIENT_SECRET){
+      return res.json(jwtResponse);
+    } else {
+      throw new Error();
+    }
   }
 
   throw new Error();
